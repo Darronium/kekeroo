71d180e32c2b37c5377d8c3391e0e7cf60136654
Darren Wan <dxlwan88@gmail.com>
2015-12-26 20:29:48 +0100

    test

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,3 +1,3 @@
-uri = URI.parse(ENV["redis://redistogo:285c284f17ddcb2075f37ec3a62d1ad0@tarpon.redistogo.com:11888/"])
+uri = URI.parse("redis://redistogo:285c284f17ddcb2075f37ec3a62d1ad0@tarpon.redistogo.com:11888/")
 REDIS = Redis.new(:url => uri)
 
